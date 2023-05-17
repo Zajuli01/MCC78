0c924d7118dd7a54511ff32faad54f6eff4f8f62
Zajuli01 <zajulitaupiqbisri@gmail.com>
2023-05-17 09:31:57 +0700

    MVC

--- View/TablesView.cs
+++ View/TablesView.cs
@@ -8,27 +8,7 @@ namespace TugasGG.View
 {
     public class TablesView
     {
-        public void Output(University university)
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Id: " + university.Id);
-            Console.WriteLine("Name: " + university.Name);
-            Console.WriteLine("-----------------------------------------");
-        }
-
-        public void Output(List<University> universities)
-        {
-            foreach (var university in universities)
-            {
-                Output(university);
-            }
-        }
-
-        public void Output(string message)
-        {
-            Console.WriteLine(message);
-        }
-
+        
         public static void GetJoinTables()
         {
             var Employee = new Employee();
